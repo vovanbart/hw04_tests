53946755881add4d9eeb3a981bbe280b6ea1a30c
vovanbart <vovanbart@icloud.com>
2021-09-24 22:45:18 +0300

    им

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -51,7 +51,7 @@ class PostURLTests(TestCase):
                 else:
                     self.assertEqual(
                         response.status_code,
-                        302,
+                        HTTPStatus.OK,
                         f'Адресс {adress} не доступен.')
 
     def test_edit_author_post(self):
